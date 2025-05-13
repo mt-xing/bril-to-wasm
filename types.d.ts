869752b68be57c4e95f33f962361a7dfc598d477
mt-xing <mt-xing@users.noreply.github.com>
2025-05-12 20:01:29 -0400

    VS Code Config

--- types.d.ts
+++ types.d.ts
@@ -1,19 +1,19 @@
 export type Type = string | {[key: string]: Type};
 export type BrilInstruction = {
-    op: string;
-    dest?: string;
-    type?: Type;
-    args?: string[];
-    funcs?: string[];
-    labels?: string[];
-    value?: number | boolean;
+  op: string;
+  dest?: string;
+  type?: Type;
+  args?: string[];
+  funcs?: string[];
+  labels?: string[];
+  value?: number | boolean;
 } | { label: string };
 export type BrilFunction = {
-    name: string;
-    args?: {name: string; type: Type}[];
-    type?: Type;
-    instrs: BrilInstruction[];
+  name: string;
+  args?: {name: string; type: Type}[];
+  type?: Type;
+  instrs: BrilInstruction[];
 };
 export type BrilProgram = {
-    functions: BrilFunction[]
+  functions: BrilFunction[]
 };
