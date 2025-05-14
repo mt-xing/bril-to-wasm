e70bc65e3b0b3759f48067c59e314acb97fdd525
mt-xing <mt-xing@users.noreply.github.com>
2025-05-13 22:39:05 -0400

    Document this script

--- compile_all_tests.ps1
+++ compile_all_tests.ps1
@@ -1,3 +1,6 @@
+# Run this script to use our compiler on every file in the input directory and
+# output the wasm to the output directory
+
 $inputDir = "./benches_briloop_json"
 $outputDir = "./benches_briloop_wat"
 
